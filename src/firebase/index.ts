e838ed9a8ca181511c0ede651dac368e6d6c7bf9
fedeWax <cha_lino_23@hotmail.com>
2024-10-28 21:31:00 -0700

    test deploy

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -10,7 +10,7 @@ const storageBucket = process.env.STORAGE_BUCKET;
 
 export const serviceAccount: admin.ServiceAccount = Object.freeze({
   projectId: process.env.PROJECT_ID,
-  privateKey: process.env.PRIVATE_KEY,
+  privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
   clientEmail: process.env.CLIENT_EMAIL,
 });
 
