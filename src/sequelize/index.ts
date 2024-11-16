4f68e840bda616605e92902cb804d376e67483af
fedeWax <cha_lino_23@hotmail.com>
2024-11-15 20:34:23 -0700

    configuracion certificado ssl

--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -7,12 +7,20 @@ import ProductModel from "../models/product";
 import UserModel from "../models/user";
 import SaleModel from '../models/sale';
 import SaleDetailsModel from '../models/saleDetails';
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_NAME;
 const host = process.env.DB_HOST;
 const port = Number(process.env.DB_PORT || 5432);
+const pathCa = process.env.DB_PATH_CA || "";
+const env = process.env.NODE_ENV;
 
 const sequelize = new Sequelize({
   dialect: PostgresDialect,
@@ -22,9 +30,11 @@ const sequelize = new Sequelize({
   host,
   port,
   models: [CategoryModel, ProductModel, InventoryModel, MovementModel, UserModel, SaleModel, SaleDetailsModel],
-  ssl: {
-    rejectUnauthorized: false
-  },
+  ssl: env === "dev"
+    ? undefined
+    : {
+      ca: readFileSync(join(__dirname, pathCa)).toString(),
+    },
 });
 
 export default sequelize;
