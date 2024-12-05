db1b93480147c3e9dffd74ed0cc959fba80147d6
fedeWax <cha_lino_23@hotmail.com>
2024-12-05 15:02:57 -0700

    sync models

--- src/routes/syncModels.ts
+++ src/routes/syncModels.ts
@@ -1,13 +1,11 @@
 import { Application, Router } from "express";
 import { syncModels } from "../controllers/syncModels";
-import sequelize from "../sequelize";
 
 const router = Router();
 
 const RouteSyncModels = async (app: Application) => {
   router.get('/', syncModels);
   app.use("/sync-models", router);
-  await sequelize.sync();
 };
 
 export default RouteSyncModels;
